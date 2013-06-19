0ffa8faf79fe2df610863930601c224e22413a01
Tobin Skinner <tobin.skinner@gmail.com>
2013-06-19 09:29:59 -0400

    Fixed thumbnail update after record; app now just adds latest thumbnail instead of all of them thumbnails again.

--- src/org/ilanguage/fielddbsessionrecorder/MyListFragment.java
+++ src/org/ilanguage/fielddbsessionrecorder/MyListFragment.java
@@ -24,38 +24,38 @@ public class MyListFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_session_overview,
 				container, false);
-		
+
 		updateThumbnails(view);
-//		File dir = Environment.getExternalStorageDirectory();
-//		String SD_PATH = dir.getAbsolutePath() + "/FieldDBSessions";
-//		File file = new File(SD_PATH);
-//		File allThumbnails[] = file.listFiles();
-//		ImageView[] imageViewArray = new ImageView[allThumbnails.length];
-//		//
-//		for (int i = 0; i < allThumbnails.length; i++) {
-//			Bitmap bmThumbnail;
-//			bmThumbnail = ThumbnailUtils.createVideoThumbnail(
-//					allThumbnails[i].getPath(), Thumbnails.MINI_KIND);
-//
-//			imageViewArray[i] = new ImageView(view.getContext());
-//
-//			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-//					LinearLayout.LayoutParams.WRAP_CONTENT,
-//					LinearLayout.LayoutParams.WRAP_CONTENT);
-//			imageViewArray[i].setLayoutParams(lp);
-//			imageViewArray[i].setPadding(5, 0, 5, 0);
-//			imageViewArray[i].setAdjustViewBounds(true);
-//			imageViewArray[i].setImageBitmap(bmThumbnail);
-//			imageViewArray[i].setTag(allThumbnails[i].getPath());
-//			imageViewArray[i].setOnClickListener(new OnClickListener() {
-//				@Override
-//				public void onClick(View v) {
-//					updateVideo(v);
-//				}
-//			});
-//			((LinearLayout) view.findViewById(R.id.thumbnailCarousel))
-//					.addView(imageViewArray[i]);
-//		}
+		// File dir = Environment.getExternalStorageDirectory();
+		// String SD_PATH = dir.getAbsolutePath() + "/FieldDBSessions";
+		// File file = new File(SD_PATH);
+		// File allThumbnails[] = file.listFiles();
+		// ImageView[] imageViewArray = new ImageView[allThumbnails.length];
+		// //
+		// for (int i = 0; i < allThumbnails.length; i++) {
+		// Bitmap bmThumbnail;
+		// bmThumbnail = ThumbnailUtils.createVideoThumbnail(
+		// allThumbnails[i].getPath(), Thumbnails.MINI_KIND);
+		//
+		// imageViewArray[i] = new ImageView(view.getContext());
+		//
+		// LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+		// LinearLayout.LayoutParams.WRAP_CONTENT,
+		// LinearLayout.LayoutParams.WRAP_CONTENT);
+		// imageViewArray[i].setLayoutParams(lp);
+		// imageViewArray[i].setPadding(5, 0, 5, 0);
+		// imageViewArray[i].setAdjustViewBounds(true);
+		// imageViewArray[i].setImageBitmap(bmThumbnail);
+		// imageViewArray[i].setTag(allThumbnails[i].getPath());
+		// imageViewArray[i].setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// updateVideo(v);
+		// }
+		// });
+		// ((LinearLayout) view.findViewById(R.id.thumbnailCarousel))
+		// .addView(imageViewArray[i]);
+		// }
 
 		return view;
 	}
@@ -72,13 +72,20 @@ public class MyListFragment extends Fragment {
 	}
 
 	public void updateThumbnails(View view) {
+
 		File dir = Environment.getExternalStorageDirectory();
 		String SD_PATH = dir.getAbsolutePath() + "/FieldDBSessions";
 		File file = new File(SD_PATH);
 		File allThumbnails[] = file.listFiles();
 		ImageView[] imageViewArray = new ImageView[allThumbnails.length];
-		//
-		for (int i = 0; i < allThumbnails.length; i++) {
+
+		int currentNumberOfThumbnails = ((LinearLayout) view
+				.findViewById(R.id.thumbnailCarousel)).getChildCount();
+		int i = 0;
+		if (currentNumberOfThumbnails > 0) {
+			i = currentNumberOfThumbnails;
+		}
+		for (; i < allThumbnails.length; i++) {
 			Bitmap bmThumbnail;
 			bmThumbnail = ThumbnailUtils.createVideoThumbnail(
 					allThumbnails[i].getPath(), Thumbnails.MINI_KIND);
@@ -103,8 +110,7 @@ public class MyListFragment extends Fragment {
 					.addView(imageViewArray[i]);
 		}
 	}
-	
-	
+
 	public interface OnItemSelectedListener {
 		public void onVideoSelect(View v);
 	}
