1f281e9bc6ec48b44525d95ada1a04085cb58fc8
Tobin Skinner <tobin.skinner@gmail.com>
2013-06-28 13:46:10 -0400

    Added gallery swipe view of all videos that shows on startup.

---
+++ src/ca/ilanguage/fielddbsessionrecorder/GalleryView.java
@@ -0,0 +1,197 @@
+package ca.ilanguage.fielddbsessionrecorder;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore.Video.Thumbnails;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+
+public class GalleryView extends Activity {
+	File videosFolder;
+	private DatumsDbAdapter mDbHelper;
+	LinearLayout carouselLayout;
+	private static final int SESSION_LIST_VIEW_ID = Menu.FIRST;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gallery);
+
+		// Create video folder if it does not already exist
+		videosFolder = new File(Environment.getExternalStorageDirectory(),
+				"FieldDBSessions");
+		videosFolder.mkdir();
+		carouselLayout = (LinearLayout) findViewById(R.id.videoPreviewCarousel);
+		populateVideoPreview(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, SESSION_LIST_VIEW_ID, 0, R.string.menu_view_session_list);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case SESSION_LIST_VIEW_ID:
+			showSessionList();
+			return true;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	public void showSessionList() {
+		Intent i = new Intent(this, SessionListView.class);
+		startActivity(i);
+	}
+
+	public void populateVideoPreview(Context c) {
+		mDbHelper = new DatumsDbAdapter(c);
+		mDbHelper.open();
+
+		File dir = Environment.getExternalStorageDirectory();
+		String SD_PATH = dir.getAbsolutePath() + "/FieldDBSessions";
+		File file = new File(SD_PATH);
+		File allVideos[] = file.listFiles();
+
+		ImageView[] imageViewArray = new ImageView[allVideos.length];
+		TextView[] textViewArray = new TextView[allVideos.length];
+		// Remove all images in view before updating
+		carouselLayout.removeAllViews();
+
+		for (int i = 0; i < allVideos.length; i++) {
+			String filePath = allVideos[i].getPath();
+			Bitmap bmVideoPreview;
+			bmVideoPreview = ThumbnailUtils.createVideoThumbnail(
+					allVideos[i].getPath(), Thumbnails.FULL_SCREEN_KIND);
+			String[] filePathParts = filePath.split("\\.");
+			String[] filePathSubParts = filePathParts[0].split("_");
+			Long rowID = Long.parseLong(filePathSubParts[3]);
+
+			// Get goal for image label
+			Cursor note = mDbHelper.fetchNote(rowID);
+
+			String tempGoal = note.getString(note
+					.getColumnIndexOrThrow(DatumsDbAdapter.KEY_FIELD1));
+
+			String goal;
+			if (tempGoal.length() > 16) {
+				goal = tempGoal.substring(0, 15).concat("...");
+			} else {
+				goal = tempGoal;
+			}
+
+			String tempDate = note.getString(note
+					.getColumnIndexOrThrow(DatumsDbAdapter.KEY_FIELD5));
+
+			// TODO Format date
+			String imageLabelText = goal.concat("\n").concat(tempDate);
+
+			imageViewArray[i] = new ImageView(c);
+			textViewArray[i] = new TextView(c);
+
+			imageViewArray[i].setTag(filePath);
+
+			// Play video in PlayVideo activity on normal click
+			imageViewArray[i].setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent playVideo = new Intent(v.getContext(),
+							PlayVideo.class);
+					playVideo.putExtra("videoFilename", v.getTag().toString());
+					startActivity(playVideo);
+				}
+			});
+
+			// Set up layout parameters
+			RelativeLayout imageAndTextRelativeLayout = new RelativeLayout(c);
+			RelativeLayout.LayoutParams main_lp = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			main_lp.addRule(RelativeLayout.CENTER_HORIZONTAL,
+					RelativeLayout.TRUE);
+			main_lp.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+
+			main_lp.setMargins(50, 50, 50, 50);
+			imageAndTextRelativeLayout.setLayoutParams(main_lp);
+
+			imageViewArray[i].setLayoutParams(main_lp);
+
+			RelativeLayout.LayoutParams text_lp = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			text_lp.addRule(RelativeLayout.CENTER_HORIZONTAL,
+					RelativeLayout.TRUE);
+			text_lp.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+			text_lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,
+					RelativeLayout.TRUE);
+			textViewArray[i].setTextColor(Color.parseColor("#FFFFFF"));
+			textViewArray[i].setTypeface(Typeface.DEFAULT_BOLD);
+			textViewArray[i].setTextSize(35);
+			textViewArray[i].setShadowLayer(1.5f, -1, 1, Color.LTGRAY);
+			textViewArray[i].setLayoutParams(text_lp);
+
+			// Add data to elements
+			Bitmap roundedBitmap = getRoundedCornerBitmap(bmVideoPreview, 50);
+			imageViewArray[i].setImageBitmap(roundedBitmap);
+			Drawable d = getResources().getDrawable(R.drawable.image_border);
+			imageViewArray[i].setBackground(d);
+
+			textViewArray[i].setText(imageLabelText);
+
+			// Add individual items to relative layout container
+			imageAndTextRelativeLayout.addView(imageViewArray[i]);
+			imageAndTextRelativeLayout.addView(textViewArray[i]);
+
+			// Add imageAndTextLinearLayout container to linear layout in view
+			carouselLayout.addView(imageAndTextRelativeLayout);
+		}
+	}
+
+	public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+		Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+				bitmap.getHeight(), Config.ARGB_8888);
+		Canvas canvas = new Canvas(output);
+
+		final int color = 0xff424242;
+		final Paint paint = new Paint();
+		final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+		final RectF rectF = new RectF(rect);
+		final float roundPx = pixels;
+
+		paint.setAntiAlias(true);
+		canvas.drawARGB(0, 0, 0, 0);
+		paint.setColor(color);
+		canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+		canvas.drawBitmap(bitmap, rect, rect, paint);
+
+		return output;
+	}
+}
---
+++ src/ca/ilanguage/fielddbsessionrecorder/SessionListView.java
@@ -0,0 +1,213 @@
+package ca.ilanguage.fielddbsessionrecorder;
+
+import java.io.File;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class SessionListView extends ListActivity {
+	private DatumsDbAdapter mDbHelper;
+	private static final int VIDEO_GALLERY_VIEW_ID = Menu.FIRST;
+	private static final int NEW_SESSION_ID = Menu.FIRST + 1;
+	private static final int DELETE_ID = Menu.FIRST + 2;
+	private static final int SESSION_ACCESS = 0;
+	private File videosFolder;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_session_recorder);
+
+		// Create videos folder if not already present
+		videosFolder = new File(Environment.getExternalStorageDirectory(),
+				"FieldDBSessions");
+		videosFolder.mkdir();
+
+		mDbHelper = new DatumsDbAdapter(this);
+		mDbHelper.open();
+		fillData();
+		registerForContextMenu(getListView());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, VIDEO_GALLERY_VIEW_ID, 0, R.string.menu_view_video_gallery);
+		menu.add(0, NEW_SESSION_ID, 0, R.string.menu_new_session);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case NEW_SESSION_ID:
+			createNote();
+			return true;
+		case VIDEO_GALLERY_VIEW_ID:
+			showVideoGallery();
+			return true;
+		}
+
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case DELETE_ID:
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+					.getMenuInfo();
+			final long row_id = info.id;
+			AlertDialog.Builder alert = new AlertDialog.Builder(this);
+			alert.setTitle(R.string.delete); // Set Alert dialog title here
+			alert.setMessage(R.string.dialog_verify_delete); // Message here
+
+			alert.setPositiveButton(R.string.delete,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,
+								int whichButton) {
+							// Delete session and all videos from that session
+							File dir = Environment
+									.getExternalStorageDirectory();
+							String SD_PATH = dir.getAbsolutePath()
+									+ "/FieldDBSessions";
+							File file = new File(SD_PATH);
+							File allVideos[] = file.listFiles();
+
+							// Should be fine to iterate over array since not
+							// removing actual array items but rather deleting
+							// from SD card
+							for (int j = 0; j < allVideos.length; j++) {
+								String filePath = allVideos[j].getPath();
+								String[] filePathParts = filePath.split("\\.");
+								String[] filePathSubParts = filePathParts[0]
+										.split("_");
+								Long rowID = Long
+										.parseLong(filePathSubParts[3]);
+								if (rowID == row_id) {
+									File fileToDelete = new File(filePath);
+									fileToDelete.delete();
+								}
+							}
+							mDbHelper.deleteNote(row_id);
+							fillData();
+						}
+					});
+
+			alert.setNegativeButton(R.string.cancel,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,
+								int whichButton) {
+							dialog.cancel();
+						}
+					});
+			AlertDialog alertDialog = alert.create();
+			alertDialog.show();
+
+			return true;
+		}
+		return super.onContextItemSelected(item);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Intent i = new Intent(this, SessionAccess.class);
+		i.putExtra(DatumsDbAdapter.KEY_ROWID, id);
+		startActivityForResult(i, SESSION_ACCESS);
+	}
+
+	private void showVideoGallery() {
+		Intent i = new Intent(this, GalleryView.class);
+		startActivity(i);
+	}
+
+	private void createNote() {
+		Long id = mDbHelper.createNote("", "", "", "", "", "");
+		Intent i = new Intent(this, SessionAccess.class);
+		i.putExtra(DatumsDbAdapter.KEY_ROWID, id);
+		startActivity(i);
+	}
+
+	private void fillData() {
+		// Get all of the rows from the database and create the item list
+		Cursor notesCursor = mDbHelper.fetchAllDatums();
+		startManagingCursor(notesCursor);
+
+		// Create an array to specify the fields we want to display in the list
+		String[] from = new String[] { DatumsDbAdapter.KEY_FIELD1,
+				DatumsDbAdapter.KEY_FIELD2, DatumsDbAdapter.KEY_FIELD3,
+				DatumsDbAdapter.KEY_FIELD4, DatumsDbAdapter.KEY_FIELD5 };
+
+		// and an array of the fields we want to bind those fields to
+		int[] to = new int[] { R.id.text1, R.id.text2, R.id.text3, R.id.text4,
+				R.id.text5 };
+
+		// Now create a simple cursor adapter and set it to display
+		MyCursorAdapter notes = new MyCursorAdapter(this,
+				R.layout.session_list_row, notesCursor, from, to, 0);
+		setListAdapter(notes);
+
+	}
+
+	// extend the SimpleCursorAdapter to create a custom class where we
+	// can override the getView to change the row colors
+	private class MyCursorAdapter extends SimpleCursorAdapter {
+
+		public MyCursorAdapter(Context context, int layout, Cursor c,
+				String[] from, int[] to, int flags) {
+			super(context, layout, c, from, to, flags);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			// get reference to the row
+			View view = super.getView(position, convertView, parent);
+			// check for odd or even to set alternate colors to the row
+			// background
+			if (position % 2 == 0) {
+				view.setBackgroundColor(Color.rgb(157, 243, 245));
+			} else {
+				view.setBackgroundColor(Color.rgb(205, 255, 255));
+			}
+			return view;
+		}
+	}
+
+	// protected void onActivityResult(int requestCode, int resultCode, Intent
+	// data) {
+	// //intent results, for those that return something
+	// }
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// Fill data on activity resumed; updates ListView of changes
+		fillData();
+	}
+
+}
