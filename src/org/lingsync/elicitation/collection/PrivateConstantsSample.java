dbf867060f1616a6f3e5f42a53d0a6295a5daacf
cesine <cesine@yahoo.com>
2013-08-28 19:17:54 -0400

    added sample constants file

---
+++ src/org/lingsync/elicitation/collection/PrivateConstantsSample.java
@@ -0,0 +1,11 @@
+package org.lingsync.elicitation.collection;
+
+public class PrivateConstants {
+	public final static String TAG = "FieldDB";
+	public final static String KEYSTORE_PASS = "hi";
+	public final static String VIDEO_UPLOADED = "there";
+	public final static String EXTRA_FILENAME = "filename";
+	public final static String EXTRA_FILEPATH = "filepath";
+	public final static String SERVER_TOKEN = "servertoken";
+	public final static String SERVER_URL = "serverurl";
+}
